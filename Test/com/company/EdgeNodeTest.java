0f125e0855b425c069947bf72491d15a0880ae49
XuanmingChen <xc222an@student.lnu.se>
2018-11-22 21:47:28 +0100

    RED

---
+++ Test/com/company/EdgeNodeTest.java
@@ -0,0 +1,52 @@
+package com.company;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import java.lang.reflect.Type;
+import java.util.NoSuchElementException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EdgeNodeTest {
+    private int dest;
+    private int distance;
+    private int fee;
+    private EdgeNode sut;
+    private EdgeNode next;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new EdgeNode(1,23,33);
+        sut.next = new EdgeNode(2,4,34);
+    }
+
+    @Test
+    public void shouldTestConstructor1(){
+        assertEquals(1,sut.dest);
+        assertEquals(23,sut.fee);
+        assertEquals(33,sut.distance);
+
+    }
+
+    @Test
+    public void shouldTestGetDest(){
+        assertEquals(1,sut.getDest());
+    }
+
+    @Test
+    public void shouldTestGetDistance(){
+        assertEquals(23,sut.getDistance());
+    }
+
+    @Test
+    public void shouldTestGetFee(){
+        assertEquals(33,sut.getFee());
+    }
+
+    @Test
+    public void shouldTestGetNext(){
+        assertEquals(2,sut.getNext().dest);
+        assertEquals(4,sut.getNext().distance);
+        assertEquals(34,sut.getNext().fee);
+    }
+
+}
--- src/com/company/EdgeNode.java
+++ src/com/company/EdgeNode.java
@@ -1,5 +1,35 @@
 package com.company;
 
 public class EdgeNode {
+    public int dest;
 
+
+    public int distance;//
+    public int fee;
+
+    public EdgeNode next;
+
+    public EdgeNode(int dest, int fee, int distance) {
+
+    }
+
+    public EdgeNode(int dest, int fee, int distance, EdgeNode next) {
+
+    }
+
+    public int getDest() {
+          return 0;
+    }
+
+    public int getDistance() {
+        return 0;
+    }
+
+    public int getFee()
+    {return 0;}
+
+    public EdgeNode getNext() {
+        return null;
+    }
 }
+
